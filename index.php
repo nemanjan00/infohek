d95eedb03db142e2ed415687993cf6f8dd3bbd6e
Nemanja Nedeljković <nemanjan00@gmail.com>
2016-04-24 20:26:31 +0200

    Update

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 	<title>Example</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="./style.css">	
+<link rel="stylesheet" type="text/css" href="http://infohek.nemanja.top/style.css">
 	 </head>
 	<body>
-		 <textarea id="source" style="display:none">	</textarea>
+		<textarea id="source" style="display:none">	</textarea>
 		<script src="https://gnab.github.io/remark/downloads/remark-latest.min.js"></script>
 		<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 		<script>
